5a94d5a4cbedefe55dd8076fa392ab1809c792c6
jon <jonathan.morales.career@gmail.com>
2024-11-14 19:59:04 -0500

    update

--- algo/MathGeo.java
+++ algo/MathGeo.java
@@ -64,24 +64,31 @@ public class MathGeo {
     }
 
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length, cols = matrix[0].length;
-        boolean[] rowZero = new boolean[rows];
-        boolean[] colZero = new boolean[cols];
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (matrix[r][c] == 0) {
-                    rowZero[r] = true;
-                    colZero[c] = true;
+        int col0 = 1;
+        int row = matrix.length;
+        int col = matrix[0].length;
+    
+        // First pass: mark rows and columns to be zeroed
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (rowZero[r] || colZero[c]) {
-                    matrix[r][c] = 0;
+    
+        // Second pass: set matrix elements to zero based on marks
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j > 0; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
+            }
+            // Check if first column needs to be set to zero
+            if (col0 == 0) {
+                matrix[i][0] = 0;
             }
         }
     }
